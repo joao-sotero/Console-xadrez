7c12e556ce60cee975c460b29c33c6f4ed7625a3
João Vitor Sotero <62224433+joao-sotero@users.noreply.github.com>
2022-08-22 12:49:35 -0300

    inicio da estrutura do tabuleiro

---
+++ xadrez-console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrez_console
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao p = new Posicao(3, 4);
+        }
+    }
+}
---
+++ xadrez-console/tabuleiro/Peca.cs
@@ -0,0 +1,19 @@
+﻿using tabuleiro;
+
+namespace xadrez_console.tabuleiro
+{
+    public class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int QteMovimentos { get; set; }
+        public Tabuleiro Tab { get; set; }
+
+        public Peca(Posicao posicao, Cor cor)
+        {
+            Posicao = posicao;
+            Cor = cor;
+            QteMovimentos = 0;
+        }
+    }
+}
