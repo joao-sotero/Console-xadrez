88b8a502258d78ead2bbdcd822a8a26caa615650
João Vitor Sotero <62224433+joao-sotero@users.noreply.github.com>
2022-08-22 21:50:17 -0300

    Classe posicaoXadrez

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -8,17 +8,8 @@ namespace xadrez_console
     {
         public static void Main(string[] args)
         {
-            var tab = new Tabuleiro(8, 8);
-
-            tab.ColocaPeca(new Torre(tab, Cor.Branca), new Posicao(0, 0));
-            tab.ColocaPeca(new Rei(tab, Cor.Branca), new Posicao(1, 9));
-            tab.ColocaPeca(new Rei(tab, Cor.Branca), new Posicao(1, 3));
-
-            tab.ColocaPeca(new Torre(tab, Cor.Branca), new Posicao(2, 4));
-
-
-
-            Tela.imprimirTabuleiro(tab);
+            var posX = new PosicaoXadrez('a', 1);
+            Console.WriteLine(posX.ToPosicao());
             Console.ReadLine(); 
         }
     }
