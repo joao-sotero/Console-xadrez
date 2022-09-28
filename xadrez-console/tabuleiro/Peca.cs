6d95df90fa66c8201e639ad1281ecf6b4b1f7d48
João Vitor Sotero <62224433+joao-sotero@users.noreply.github.com>
2022-09-28 15:44:36 -0300

    implementado restrição de movimentos

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -28,6 +28,23 @@ namespace xadrez_console.tabuleiro
             return p == null || p.Cor != this.Cor;
         }
 
+        public bool ExisteMovimentosPossiveis()
+        {
+            bool[,] movimentosPossiveis = MovimentosPossiveis();
+            for(int i = 0; i < Tab.Linhas; i++) 
+                for(int j = 0; j < Tab.Colunas; j++)
+                {
+                    if (movimentosPossiveis[i, j])
+                        return true;
+                }
+            return false;
+        }
+
+        public bool PodeMoverPara(Posicao pos)
+        {
+            return MovimentosPossiveis()[pos.Linha, pos.Coluna];
+        }
+
         public abstract bool[,] MovimentosPossiveis();
     }
 }
--- xadrez-console/xadrez/PartidaXadrez.cs
+++ xadrez-console/xadrez/PartidaXadrez.cs
@@ -7,8 +7,8 @@ namespace xadrez
     public class PartidaXadrez
     {
         public Tabuleiro Tab { get;  private set; }
-        public int Turno { get; set; }
-        public Cor JogadorAtual { get; set; }
+        public int Turno { get; private set; }
+        public Cor JogadorAtual { get; private set; }
         public bool Terminada { get; set; }
 
         public PartidaXadrez()
@@ -26,6 +26,37 @@ namespace xadrez
             Tab.ColocaPeca(new Torre(Tab, Cor.Preta), new PosicaoXadrez('d', 1).ToPosicao());
             Tab.ColocaPeca(new Torre(Tab, Cor.Branca), new PosicaoXadrez('e', 1).ToPosicao());
             Tab.ColocaPeca(new Torre(Tab, Cor.Branca), new PosicaoXadrez('f', 1).ToPosicao());
+        }
+
+        public void RealizaJogada(Posicao origem, Posicao destino)
+        {
+            ExecutaMovimento(origem, destino);
+            Turno++;
+            MudarJogador();
+        }
+
+        public void ValidarPosicaoOrigem(Posicao pos)
+        {
+            if (Tab.Peca(pos) == null)
+                throw new TabuleiroException("Não Existe peça na posição de origem escolhida");
+            if (JogadorAtual != Tab.Peca(pos).Cor)
+                throw new TabuleiroException("A peça de origem escolhida é do adiversario");
+            if (!Tab.Peca(pos).ExisteMovimentosPossiveis())
+                throw new TabuleiroException("Não há movimentos possíveis para a peça de origem escolhida");
+        }
+
+        public void ValidarPosicaoDestino(Posicao origem, Posicao destino)
+        {
+            if (!Tab.Peca(origem).PodeMoverPara(destino))
+                throw new TabuleiroException("Posição de destino invalida");
+        }
+
+        private void MudarJogador()
+        {
+            if (JogadorAtual == Cor.Branca)
+                JogadorAtual = Cor.Preta;
+            else
+                JogadorAtual = Cor.Branca;
         }
 
         public void ExecutaMovimento(Posicao origem, Posicao destino)
